406abc44ef3e898c258233dc646f3cb676b1c90d
Abdul Hadhi <108044807+hadhi-dev@users.noreply.github.com>
2026-03-18 00:06:56 +0530

    Updated Profile Page

---
+++ assets/analyticsService-W8y_xzny.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-Ca0c9rRZ.js";const l={trackHajLabel(c,a=null){const t={event_type:c};a&&(t.cover_id=a),e.post("haj/analytics",t,{auth:!1}).catch(()=>{})},getHajLabelAnalytics:()=>e.get("haj/analytics")};export{l as a};
